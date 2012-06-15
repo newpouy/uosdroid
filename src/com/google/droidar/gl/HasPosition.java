9b60d4ef5a680de380a55485dc2dfe18cd5ea38a
ricardoflandrade@gmail.com <ricardoflandrade@gmail.com@a8c7f073-df90-9037-35e3-936e18abfa86>
2012-06-15 02:42:12 +0000

    Deleção dos pacotes do DroidAR

--- src/com/google/droidar/gl/HasPosition.java
+++
@@ -1,8 +0,0 @@
-package com.google.droidar.gl;
-
-import com.google.droidar.util.Vec;
-
-public interface HasPosition {
-	public Vec getPosition();
-	public void setPosition(Vec position);
-}
--- src/com/google/droidar/gl/HasScale.java
+++
@@ -1,9 +0,0 @@
-package com.google.droidar.gl;
-
-import com.google.droidar.util.Vec;
-
-public interface HasScale {
-	public Vec getScale();
-
-	public void setScale(Vec scale);
-}
